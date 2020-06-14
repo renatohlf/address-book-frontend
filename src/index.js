14b63bc7dab11ab012ab717b3032a8237f3e577f
Renato Farias <renatohlf@gmail.com>
2020-06-14 13:24:24 +0200

    Adding sentry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+Sentry.init({dsn: "https://f6057aa8ff0b42f4ac13bacb835dc604@o407076.ingest.sentry.io/5275623"});
 
 ReactDOM.render(
     <React.StrictMode>
